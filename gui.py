61970c43625958637dbcbfa128075c4f8aea1b4c
BogdanChisu <bchisu@gmail.com>
2024-12-10 20:59:46 +0200

    Added complete and add button images

--- gui.py
+++ gui.py
@@ -7,11 +7,15 @@ sg.theme("Black")
 clock = sg.Text("", key="clock")
 label  = sg.Text("Type in a to-do: ")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
-add_button = sg.Button("Add")
+add_button = sg.Button(size=4, image_source="imgs/add.png",
+                       mouseover_colors="LightBlue2", tooltip="Add todo",
+                       key="Add")
 list_box = sg.Listbox(values=functions.get_todos(),
                       key='todos', enable_events=True, size=[45, 10])
 edit_button = sg.Button("Edit")
-complete_button = sg.Button("Complete")
+complete_button = sg.Button(size=4, image_source="imgs/complete.png",
+                       mouseover_colors="LightBlue2", tooltip="Complete todo",
+                       key="Complete")
 exit_button = sg.Button("Exit")
 
 layout = [[clock],
