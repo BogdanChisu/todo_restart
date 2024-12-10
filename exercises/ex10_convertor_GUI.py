2c1b8f38bfe733f6142da16c772a4049440da5c2
BogdanChisu <bchisu@gmail.com>
2024-12-10 21:31:51 +0200

    added "Exit" button to "Converter GUI app"

--- exercises/ex10_convertor_GUI.py
+++ exercises/ex10_convertor_GUI.py
@@ -7,11 +7,12 @@ label_inches = sg.Text("Enter inches: ")
 input_inches = sg.Input(key="inches")
 
 convert_button = sg.Button("Convert")
+exit_button = sg.Button("Exit")
 output_result = sg.Text(key="result")
 
 layout1 = [[label_feet, input_feet],
           [label_inches, input_inches],
-          [convert_button, output_result]]
+          [convert_button, exit_button, output_result]]
 
 window = sg.Window("Convertor", layout=layout1)
 
@@ -23,6 +24,8 @@ while True:
             inches_val = float(values['inches'])
             result_val = feet_val * 0.3048 + inches_val* 0.0254
             window["result"].update(value=str(result_val)+" m")
+        case "Exit":
+            exit()
         case sg.WIN_CLOSED:
             break
 
